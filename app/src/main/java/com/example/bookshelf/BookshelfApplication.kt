8d1e4cd135f7521f222b967576cc844eae55a530
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-03-21 15:34:25 -0400

    Add dependency injection

---
+++ app/src/main/java/com/example/bookshelf/BookshelfApplication.kt
@@ -0,0 +1,14 @@
+package com.example.bookshelf
+
+import android.app.Application
+import com.example.bookshelf.data.AppContainer
+import com.example.bookshelf.data.DefaultContainer
+
+class BookshelfApplication : Application() {
+    private lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultContainer()
+    }
+}
---
+++ app/src/main/java/com/example/bookshelf/data/AppContainer.kt
@@ -0,0 +1,24 @@
+package com.example.bookshelf.data
+
+import com.example.bookshelf.network.BooksApiService
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+interface AppContainer {
+    val booksRepository: BooksRepository
+}
+
+class DefaultContainer : AppContainer {
+    private val baseUrl = "https://www.googleapis.com/books/v1"
+
+    private val retrofitService = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    private val retrofit = retrofitService.create(BooksApiService::class.java)
+
+    override val booksRepository: BooksRepository by lazy {
+        NetworkBookRepository(retrofit)
+    }
+}
